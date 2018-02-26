9537c731dbe44031de5fad1d023530c24a9a9dd3
emaasit <daniel.emaasit@gmail.com>
2018-02-26 12:02:39 -0500

    now using VMT

--- code/notebooks/setup_bayes.py
+++ code/notebooks/setup_bayes.py
@@ -10,14 +10,14 @@ import matplotlib.pyplot as plt
 # from bqplot import pyplot as plt
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
-from beakerx import *
+# from beakerx import *
 sns.set_context('notebook', font_scale = 1.1)
 np.random.seed(12345)
 rc = {'xtick.labelsize': 20, 'ytick.labelsize': 20, 'axes.labelsize': 20, 'font.size': 20, 'lines.linewidth': 2.0, 
       'lines.markersize': 8, 'font.family': "serif", 'font.serif': "cm", 'savefig.dpi': 200,
       'text.usetex': False, 'legend.fontsize': 20.0, 'axes.titlesize': 20, "figure.figsize": [20, 12]}
 sns.set(rc = rc)
-sns.set_style("whitegrid")
+sns.set_style("ticks")
 from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
 import gpflow
