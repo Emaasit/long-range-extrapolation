2a6be23c0f424f23b680eb0fbc1d05c973abc61c
emaasit <daniel.emaasit@gmail.com>
2018-02-26 21:36:04 -0500

    calculated performance measures

--- code/notebooks/setup_bayes.py
+++ code/notebooks/setup_bayes.py
@@ -13,9 +13,9 @@ import ipywidgets as widgets
 # from beakerx import *
 sns.set_context('notebook', font_scale = 1.1)
 np.random.seed(12345)
-rc = {'xtick.labelsize': 20, 'ytick.labelsize': 20, 'axes.labelsize': 20, 'font.size': 20, 'lines.linewidth': 2.0, 
+rc = {'xtick.labelsize': 40, 'ytick.labelsize': 40, 'axes.labelsize': 40, 'font.size': 40, 'lines.linewidth': 2.0, 
       'lines.markersize': 8, 'font.family': "serif", 'font.serif': "cm", 'savefig.dpi': 200,
-      'text.usetex': False, 'legend.fontsize': 20.0, 'axes.titlesize': 20, "figure.figsize": [20, 12]}
+      'text.usetex': False, 'legend.fontsize': 40.0, 'axes.titlesize': 40, "figure.figsize": [28, 18], "savefig.dpi": 300}
 sns.set(rc = rc)
 sns.set_style("ticks")
 from IPython.core.interactiveshell import InteractiveShell
